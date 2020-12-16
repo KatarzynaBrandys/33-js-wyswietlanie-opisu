89389f8bdcecdc5e35a576ffadbc1e573552e194
KatarzynaBrandys <68773576+KatarzynaBrandys@users.noreply.github.com>
2020-12-16 18:43:13 +0100

    update

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let paragraf = document.getElementById("opis");
+let button = document.querySelector("button");
+
+document.querySelector("button").onclick = function () {
+    paragraf.textContent = "Dodany tekst";
+}
